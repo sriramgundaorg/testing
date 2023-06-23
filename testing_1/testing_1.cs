95879345277e740c0a40b13d459a13a68048d516
gps19 <gps19@LAPTOP-VL59QLK5>
2023-06-23 21:04:38 +0800

    Add project files.

---
+++ testing_1/testing_1.cs
@@ -0,0 +1,23 @@
+namespace Testing_1
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Globalization;
+	using System.Text;
+	using Skyline.DataMiner.Automation;
+	
+	/// <summary>
+	/// Represents a DataMiner Automation script.
+	/// </summary>
+	public class Script
+	{
+		/// <summary>
+		/// The script entry point.
+		/// </summary>
+		/// <param name="engine">Link with SLAutomation process.</param>
+		public void Run(IEngine engine)
+		{
+			engine.GenerateInformation("Sriram Testing priject");
+		}
+	}
+}
